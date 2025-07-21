db0d47a5e126ffc4590cc34308527bdee87eefe5
Kauã Klassmann <kaua.klassmann661@gmail.com>
2025-07-21 17:28:07 -0300

    Added route to get resume attachment

--- backend/src/routes/resume.rs
+++ backend/src/routes/resume.rs
@@ -1,4 +1,5 @@
 use axum::{Router, routing::post};
+use tower_http::services::ServeDir;
 
 use crate::{handlers, state::AppState};
 
@@ -7,4 +8,5 @@ pub(super) fn configure_routes() -> Router<AppState> {
         .route("/create", post(handlers::resume::create))
         .route("/{resume_id}/edit", post(handlers::resume::edit))
         .route("/{resume_id}/upload", post(handlers::resume::upload))
+        .fallback_service(ServeDir::new("uploads/resume"))
 }
